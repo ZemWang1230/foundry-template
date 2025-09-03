cffb1100a7f2f5f5adcb9ddf2e4a4c14fb9f9277
WZM <zhemingwang123@gmail.com>
2025-09-03 21:08:45 +0800

    init-foundry-template

---
+++ test/Test.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+// contract Test is Test {
+//     function setUp() public {
+
+//     }
+
+//     function test_XXX() public {
+
+//     }
+// }
