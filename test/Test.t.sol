425df934f7026ef4628c74aba04475e009bc3b8c
WZM <zhemingwang123@gmail.com>
2025-09-04 18:52:19 +0800

    init-foundry-template

--- test/Test.t.sol
+++ test/Test.t.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import "forge-std/Vm.sol";
 
 contract TestTest is Test {
+    Vm vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
     function setUp() public {}
 
     function test_XXX() public {}
